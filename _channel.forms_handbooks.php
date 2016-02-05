f697655e78cad553100e5155d12a2c8330323244
Michael C Gorman <michael@michaelcgorman.net>
2016-02-05 13:57:54 -0600

    Create Forms & Handbooks content

---
+++ _channel.forms_handbooks.php
@@ -0,0 +1,8 @@
+<p><a href="https://docs.google.com/a/iwu.edu/spreadsheet/viewform?usp=drive_web&formkey=dFJWd1ZQYXh1ajNjbjhpdGtLZ3JSb3c6MQ#gid=0">Bias Incident Report</a></p>
+<p><a href="https://www.iwu.edu/bus/accountspay/Accounts_Forms.html">Business Office Forms</a></p>
+<p><a href="https://www.iwu.edu/iwujobs/exempt-handbook.pdf">Exempt Staff Handbook (PDF)</a></p>
+<p><a href="https://www.iwu.edu/provost/faculty-handbook.pdf">Faculty Handbook (PDF)</a></p>
+<p><a href="https://www.iwu.edu/iwujobs/hrforms.html">Human Resources</a></p>
+<p><a href="https://www.iwu.edu/iwujobs/nonexempt-handbook.pdf">Non-Exempt Staff Handbook (PDF)</a></p>
+<p><a href="https://php.iwu.edu/concern/">Student Concern Form</a></p>
+<p><a href="https://www.iwu.edu/judicial/handbook/">Student Handbook</a></p>
