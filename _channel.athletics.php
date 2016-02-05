4e8134db5a596f3d7026557657bc1bc4718d1c4c
Michael C Gorman <michael@michaelcgorman.net>
2016-02-05 14:50:19 -0600

    Clean up Athletics channel

--- _channel.athletics.php
+++ _channel.athletics.php
@@ -1,37 +1,13 @@
-<table style="width: 100%;" border="0">
-<tbody>
-<tr>
-<td><span id="p_chan_title_txt" class="uportal-text"><strong><a id="p_chan_links_lnk" onmouseover="window.status=''; return true;" href="http://www.iwusports.com/" target="target=&quot;_blank&quot;">Football Season is Almost Here!</a></strong></span></td>
-</tr>
-<tr>
-<td>
-<table border="0">
-<tbody>
-<tr>
-<td colspan="2"><img src="https://my.iwu.edu/cps/images/misc/dot-blank.gif" border="0" alt="" width="1" height="5" /></td>
-</tr>
-<tr>
-<td><img src="https://my.iwu.edu/cps/images/misc/dot-blank.gif" border="0" alt="" width="8" height="1" /></td>
-<td align="center"><img id="p_chan_img" src="https://my.iwu.edu/site/content/2734922570709747.jpg" border="0" alt="" /><br /> <img src="https://my.iwu.edu/cps/images/misc/dot-blank.gif" border="0" alt="" width="1" height="5" /><br /> <span id="p_chan_img_cap" class="uportal-text-small"></span></td>
-</tr>
-<tr>
-<td colspan="2"><img title="" src="https://my.iwu.edu/cps/images/misc/dot-blank.gif" alt="" width="1" height="5" /></td>
-</tr>
-</tbody>
-</table>
+<h2><a href="http://www.iwusports.com/">Football Season is Almost Here!</a></h2>
+<img src="https://my.iwu.edu/site/content/2734922570709747.jpg" /><br />
 <p>Here are some important things to know before you experience your first game.</p>
 <ul>
-<li>Games are for your student with ID and $6 for adults.</li>
-<li>Tailgating is encouraged. Arrive early, break out your portable grill and favorite BBQ dishes and enjoy the ambiance of 3,000+ like minded Titan fans!</li>
-<li>&nbsp;Wilder Field (Tucci Stadium) is located behind the Shirk Center where ample parking is available. <a href="http://maps.google.com/maps?q=Shirk+Center,+East+Emerson+Street,+B
+<li>Games are free for students with ID and $6 for adults.</li>
+<li>Tailgating is encouraged. Arrive early, break out your portable grill and favorite BBQ dishes and enjoy the ambiance of 3,000+ like-minded Titan fans!</li>
+<li>Wilder Field (Tucci Stadium) is located behind the Shirk Center where ample parking is available. <a href="http://maps.google.com/maps?q=Shirk+Center,+East+Emerson+Street,+B
 loomington,+IL&amp;hl=en&amp;sll=40.493234,-88.989951&amp;sspn=0.02105,0
 .048752&amp;oq=Shir&amp;t=h&amp;hq=Shirk+Center,&amp;hnear=E+Emerson+St,
 +Bloomington,+McLean,+Illinois+61701&amp;z=16">Google Maps &amp; driving directions</a>.</li>
-<li>The Titan band is loud! You may want to sit further away from the west end of the Tucci Stadium (where they are normally located) if loud music is not your cup of tea. Otherwise, enjoy the upbeat music!&nbsp; :)</li>
+<li>The Titan band is loud! You may want to sit further away from the west end of the Tucci Stadium (where they are normally located) if loud music is not your cup of tea. Otherwise, enjoy the upbeat music! :)</li>
 <li>Can't make it to one of the games? Most home games can be <a href="http://client.stretchinternet.com/client/iwu.portal">viewed online here</a>.</li>
 </ul>
-<p>&nbsp;</p>
-</td>
-</tr>
-</tbody>
-</table>
