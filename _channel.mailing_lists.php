5c5d0e82135abf7f049a1d35a7d17cc8890b1f69
Michael C Gorman <michael@michaelcgorman.net>
2016-02-05 14:03:53 -0600

    Create Mailing Lists content

---
+++ _channel.mailing_lists.php
@@ -0,0 +1,7 @@
+<a href="http://lists.iwu.edu/mailman/listinfo/facforum">Faculty Forum</a>
+<br/> 
+<a href="http://lists.iwu.edu/mailman/listinfo/staforum">Staff Forum</a>
+<br/> 
+<a href="http://lists.iwu.edu/mailman/listinfo/s-senate">Student Senate</a>
+<br/> 
+<a href="http://lists.iwu.edu/mailman/listinfo">All Mailing Lists</a>
