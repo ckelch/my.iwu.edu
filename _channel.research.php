23529ca26c535ba526799d9da6f2c120dca26e39
Michael C Gorman <michael@michaelcgorman.net>
2016-02-05 14:01:26 -0600

    Create Research content

---
+++ _channel.research.php
@@ -0,0 +1,17 @@
+<a href="/action/">Action Research Center</a>
+<br/> 
+<a href="http://digitalcommons.iwu.edu/">Digital Commons</a>
+<br/> 
+<a href="/research/faculty.html">Faculty Research &amp; Creative Activity</a>
+<br/> 
+<a href="/grants/">Grants and Foundation Relations</a>
+<br/> 
+<a href="/instres/">Institutional Research &amp; Planning</a>
+<br/> 
+<a href="/irb/">Institutional Review Board</a>
+<br/> 
+<a href="/jwprc/">John Wesley Powell Research Conference</a>
+<br/> 
+<a href="https://iwu.us2.qualtrics.com/ControlPanel/">Qualtrics</a>
+<br/> 
+<a href="/research/Undergraduate_Journals.html">Undergraduate Journals</a>
