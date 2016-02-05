8aba7b85481f082b586f470c2df584ac799b30bd
Michael C Gorman <michael@michaelcgorman.net>
2016-02-05 14:06:15 -0600

    Create Campus Community & Councils content

---
+++ _channel.councils.php
@@ -0,0 +1,11 @@
+<a href="/bikes/">Bikes</a>
+<br/> 
+<a href="/diversity/">Diversity, University Council for</a>
+<br/> 
+<a href="/greenetwork/">GREENetwork</a>
+<br/> 
+<a href="/innovation/">Innovation Fund</a>
+<br/> 
+<a href="/facresources/NonOrg.html">Non-Org</a>
+<br/> 
+<a href="/facresources/council/">Staff Council</a>
